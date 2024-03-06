98d4628be38cdf08740b16a8b191a6b42c94c5ef
Udit Rawat <uditrawat1905@gmail.com>
2024-03-06 19:53:07 +0530

    final push of all the models and packages

--- app.py
+++ app.py
@@ -2,66 +2,127 @@ from model import predictor
 from sklearn.model_selection import train_test_split
 import streamlit as st
 import pandas as pd
-st.set_page_config(layout="wide")
 
 
-st.title("Quick-Profile")
-st.subheader("Get a quick profile of models that work best with your data.")
+def main():
+    st.set_page_config(layout="centered")
+    st.markdown("<h1 style='text-align: center;'>QUICK-PROFILER</h1>",
+                unsafe_allow_html=True)
+    st.subheader(
+        "Get a quick profile of models that work best with your data.")
 
-uploaded_file = st.file_uploader("Upload CSV file")
+    uploaded_file = st.file_uploader("Upload CSV file")
 
-try:
-    if uploaded_file is not None:
-        if uploaded_file.name.endswith('.csv'):
-            df = pd.read_csv(uploaded_file)
+    try:
+        if uploaded_file is not None:
+            if uploaded_file.name.endswith('.csv'):
+                # Read the dataset
+                df = pd.read_csv(uploaded_file)
 
-            view_filter = st.selectbox("View Filtered Data", [
-                                       "Yes", "No"], key="filter")
-            if view_filter == "Yes":
-                # Check if columns are eligible for regression
-                non_continuous_columns = []
-                for column in df.columns:
-                    if df[column].dtype != 'float64' and df[column].dtype != 'int64':
-                        non_continuous_columns.append(column)
+                # Implement limiter to restrict dataset size
+                if len(df) > 10000:
+                    st.warning(
+                        "The uploaded dataset contains more than 10,000 rows. Only the first 10,000 rows will be used.")
+                    df = df.head(10000)
 
-                # Filter the dataframe by dropping non-continuous columns
-                df = df.drop(columns=non_continuous_columns, axis=1)
-
+                st.markdown("---")
                 view_data = st.selectbox(
-                    "View Data", ["Yes", "No"], key="data")
+                    "View Data", ["Select an option", "Yes", "No"], index=0)
                 if view_data == "Yes":
+                    st.markdown(
+                        "<style>div[data-testid='stDataFrame'] div{margin: 0 auto;}</style>", unsafe_allow_html=True)
                     st.write(df)
-            view_data = st.selectbox(
-                "View Data", ["Yes", "No"], key="data_normal")
-            if view_data == "Yes":
-                st.write(df)
 
-            columns = df.columns.tolist()
+                elif view_data == "No":
+                    pass  # Do nothing if "No" is selected
 
-            target = st.selectbox("Select Target variable", [
-                                  ""] + columns, index=0, key="target")
-            labels = st.multiselect("Select Labels", columns, key="labels")
-            if labels and target:
-                class_choice = st.selectbox("Select Classification or Regression", [
-                    "Classification", "Regression"], key="class_choice")
+                st.markdown("---")
 
-                # Ensure target and labels are lists of column names
-                target = [target]
-                labels = list(labels)
+                # Auto-detect the type of data based on dtype of target variable
+                target, labels = select_variables(df.columns.tolist(), df)
+                if target:
+                    if df[target].dtype in ['object', 'int']:
+                        class_choice = st.selectbox("Select Classification or Regression", [
+                                                    "Classification", "Regression"])
+                    else:
+                        class_choice = "Regression"
 
-                # Select the specified columns as DataFrames
-                X = df[labels]
-                y = df[target]
+                    # Add a button to initiate the predictor
+                    if st.button("Run"):
+                        # Show a progress bar while the model is being trained
+                        progress_bar = st.progress(0)
+                        for percent_complete in range(100):
+                            progress_bar.progress(percent_complete + 1)
 
-                # Split the data
-                X_train, X_test, y_train, y_test = train_test_split(
-                    X, y, random_state=42, test_size=0.2)
+                            X, y = df[labels], df[target]
+                            X_train, X_test, y_train, y_test = train_test_split(
+                                X, y, random_state=108, test_size=0.2)
 
-                models = predictor(X_train, X_test, y_train,
-                                   y_test, class_choice=class_choice)
-                st.text(models)
+                        # Perform prediction
+                        models = predictor(
+                            X_train, X_test, y_train, y_test, class_choice=class_choice)
 
-        else:
-            st.error("Please upload a CSV file.")
-except Exception as e:
-    st.error(f"An error occurred: {str(e)}")
+                        # Show toast message upon completion
+                        st.success("Prediction completed successfully!")
+
+                        # Display the results
+                        st.write_stream(models)
+                else:
+                    st.warning("Please select a target variable.")
+
+            else:
+                st.error("Please upload a CSV file.")
+
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+
+    # Footer HTML
+    footer_html = """
+    <style>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #f05941;
+        color: white;
+        text-align: right;
+        padding: 10px;
+        display: flex;
+        justify-content: flex-end;
+    }
+    .footer img {
+        border: none;
+        margin-left: 10px;
+    }
+    </style>
+    <div class="footer">
+        <a href="https://www.linkedin.com/in/udit-rawat-65204a279/" target="_blank" style="padding: 5px;">
+            <img src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg" width="20" height="20" style="vertical-align: middle;" alt="LinkedIn">
+        </a>
+        <a href="https://github.com/udit-rawat" target="_blank" style="padding: 5px;">
+            <img src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/github.svg" width="20" height="20" style="vertical-align: middle;" alt="GitHub">
+        </a>
+        <a href="mailto:uditcsrawat@gmail.com" target="_blank" style="padding: 5px;">
+            <img src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/gmail.svg" width="20" height="20" style="vertical-align: middle;" alt="Email">
+        </a>
+    </div>
+    """
+
+    # Display the footer
+    st.markdown(footer_html, unsafe_allow_html=True)
+
+
+def select_variables(columns, df):
+    target = st.selectbox("Select Target variable", [
+                          ""] + columns, index=0, key="target")
+    if target:
+        labels = st.multiselect(
+            "Select Labels", [col for col in columns if col != target], key="labels")
+    else:
+        labels = []
+    return target, labels
+
+
+if __name__ == "__main__":
+    main()
--- model.py
+++ model.py
@@ -1,14 +1,17 @@
-
-from lazypredict.Supervised import LazyClassifier
-from lazypredict.Supervised import LazyRegressor
+from lazypredict.Supervised import LazyClassifier, LazyRegressor
+import pandas as pd
 
 
 def predictor(X_train, X_test, y_train, y_test, class_choice):
+    pd.set_option('display.max_columns', None)  # Display all columns
+    pd.set_option('display.width', None)  # Remove column width restriction
+    pd.set_option('display.float_format', lambda x: '%.2f' %
+                  x)  # Set float format
     if class_choice == "Classification":
         clf = LazyClassifier(
-            verbose=0, ignore_warnings=True, custom_metric=None)
+            verbose=2, ignore_warnings=True, custom_metric=None)
     else:
         clf = LazyRegressor(
-            verbose=0, ignore_warnings=True, custom_metric=None)
+            verbose=2, ignore_warnings=True, custom_metric=None)
     models = clf.fit(X_train, X_test, y_train, y_test)
     return models
